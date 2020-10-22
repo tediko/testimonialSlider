becf3333a1e1c068428d02c48126f27a6be56d0b
tediko <tediko123@wp.pl>
2020-10-22 12:26:37 +0200

    Added slider querySelectors

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const slider = document.querySelector('.slider');
+const track = document.querySelector('.slider__track');
+const nextButton = document.querySelector('.slider__button-next');
+const prevButton = document.querySelector('.slider__button-prev');
+let slides = document.querySelectorAll('.slider__slide');
+let index = 1;
