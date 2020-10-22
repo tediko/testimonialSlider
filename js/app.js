892c7079ea01f056accba0b27a50113cf292c929
tediko <tediko123@wp.pl>
2020-10-22 12:27:08 +0200

    Clone first and last slide and append it to track

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ const nextButton = document.querySelector('.slider__button-next');
 const prevButton = document.querySelector('.slider__button-prev');
 let slides = document.querySelectorAll('.slider__slide');
 let index = 1;
+
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
+firstClone.id = `first-clone`;
+lastClone.id = `last-clone`;
+track.append(firstClone);
+track.prepend(lastClone);
