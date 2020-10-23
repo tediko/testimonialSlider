9d3791a7ddf8f721c40e4195866b0f89eba9aa7d
tediko <tediko123@wp.pl>
2020-10-23 12:33:51 +0200

    Added function to change indicators

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const slider = document.querySelector('.slider__container');
 const track = document.querySelector('.slider__track');
 const buttons = document.querySelectorAll('[data-controls]');
+const indicators = document.querySelectorAll('[data-idx]');
 let slides = document.querySelectorAll('.slider__slide');
 const delay = 3000;
 let index = 1;
+let current = 0;
 let slideInterval;
 
 const firstClone = slides[0].cloneNode(true);
@@ -41,15 +43,43 @@ const isTransitionend = () => {
 
 const moveToNextSlide = () => {
     if (index >= slides.length - 1) return;
+    checkCurrent(true);
+    changeIndicators();
     index++;
     track.style.transform = `translateX(${-slideWidth * index}px)`;
     track.style.transition = `transform 250ms ease-in-out`;
 }
 const moveToPrevSlide = () => {
     if (index <= 0) return;
+    checkCurrent(false);
+    changeIndicators();
     index--;
     track.style.transform = `translateX(${-slideWidth * index}px)`;
     track.style.transition = `transform 250ms ease-in-out`;
+}
+
+const changeIndicators = () => {
+    removeAllIndicators();
+    indicators.forEach(indicator => {
+        if (indicator.dataset.idx == current) {
+            indicator.classList.add('active');
+        }
+    })
+}
+
+const checkCurrent = (check) => {
+    check ? current++ : current--;
+    if (current > 3) {
+        current = 0;
+    } else if (current < 0) {
+        current = 3;
+    } 
+}
+
+const removeAllIndicators = () => {
+    indicators.forEach(indicator => {
+        indicator.classList.remove('active');
+    })
 }
 
 
@@ -62,4 +92,4 @@ buttons.forEach(button => {
         const buttonDir = button.dataset.controls;
         buttonDir == 'prev' ? moveToPrevSlide() : moveToNextSlide();
     })
-}) 
+})
